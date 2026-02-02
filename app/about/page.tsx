885a3053429ab50141d88d24f2238b1052e7fd9c
Angelinaleib <angelinaaleib@gmail.com>
2026-02-02 14:11:33 +0100

    New about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,11 @@ export default function AboutPage() {
     <div>
       <h1 className = "text-2xl font-medium text-blue-300 mb-4" >Hi, Im Angelina! </h1>
       
-      <p>I am currently a student at University of Duisburg-Essen, studying information systems. During university I worked as a data analyst at a SaaS-Startup.</p>
+      <p>I am currently sutdying business informatics at the University of Duisburg-Essen, Essen. 
+        I have practical experience from working as a student data analyst in a SaaS startup, where I supported data analysis, reporting, and system-related decision processes.
+        My background is in business informatics, with a strong interest in how digital systems shape human behavior, perception, and decision-making.
+        Currently, I explore topics such as data bias, AI ethics, and human–technology interaction through small projects and short essays. I’m especially interested in reflective, human-centered approaches to technology rather than purely technical solutions.
+        Alongside this, I maintain a creative practice, using visual work as a way to think, observe, and experiment beyond formal structures.</p>
     </div>
   );
 }
