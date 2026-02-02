4fa6be7bdcb49199fa6301d277952db0a6468c6d
Angelinaleib <angelinaaleib@gmail.com>
2026-02-02 14:19:31 +0100

    New Home

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,7 +3,7 @@ export default function AboutPage() {
     <div>
       <h1 className = "text-2xl font-medium text-blue-300 mb-4" >Hi, I'm Angelina! </h1>
       
-      <p className="mb-4"> I'm currently sutdying business informatics at the University of Duisburg-Essen, Essen. 
+      <p className="mb-4"> I'm currently studying business informatics at the University of Duisburg-Essen, Essen. 
         I have practical experience from working as a student data analyst in a SaaS startup, where I supported data analysis, reporting, and system-related decision processes.</p
        
       <p className="mb-4"> My background is in business informatics, with a strong interest in how digital systems shape human behavior, perception, and decision-making.
