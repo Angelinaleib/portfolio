899b26723c05307e4b1b40b96b8fad8ec4ce984a
Angelinaleib <angelinaaleib@gmail.com>
2026-02-02 21:15:18 +0100

    homepage

--- app/page.tsx
+++ app/page.tsx
@@ -4,11 +4,15 @@ export default function Home() {
   return (
     <div className="space-y-6">
       <h1 className="text-2xl font-bold text-blue-500">Welcome to my portfolio!</h1>
+
+      <p>I'm a student of business informatics at the University of Duisburg-Essen, Essen. 
+        I'm fascinated by the intersection of technology and human behavior.</p>
+      <p>Feel free to explore my projects and get in touch!</p>
       <a
         className="inline-block rounded-md bg-blue-600 px-5 py-2.5 text-sm font-semibold text-white transition hover:bg-blue-700"
         href="mailto:your@email.com"
       >
-        Contact me
+        Say hi
       </a>
     </div>
   );
