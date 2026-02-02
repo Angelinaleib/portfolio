8ce3ef4a153360299e1466adc9e5a5b8a25455b6
Angelinaleib <angelinaaleib@gmail.com>
2026-02-02 14:16:16 +0100

    New about2

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,13 +1,15 @@
 export default function AboutPage() {
   return (
     <div>
-      <h1 className = "text-2xl font-medium text-blue-300 mb-4" >Hi, Im Angelina! </h1>
+      <h1 className = "text-2xl font-medium text-blue-300 mb-4" >Hi, I'm Angelina! </h1>
       
-      <p>I am currently sutdying business informatics at the University of Duisburg-Essen, Essen. 
-        I have practical experience from working as a student data analyst in a SaaS startup, where I supported data analysis, reporting, and system-related decision processes.
-        My background is in business informatics, with a strong interest in how digital systems shape human behavior, perception, and decision-making.
-        Currently, I explore topics such as data bias, AI ethics, and human–technology interaction through small projects and short essays. I’m especially interested in reflective, human-centered approaches to technology rather than purely technical solutions.
-        Alongside this, I maintain a creative practice, using visual work as a way to think, observe, and experiment beyond formal structures.</p>
+      <p className="mb-4"> I'm currently sutdying business informatics at the University of Duisburg-Essen, Essen. 
+        I have practical experience from working as a student data analyst in a SaaS startup, where I supported data analysis, reporting, and system-related decision processes.</p
+       
+      <p className="mb-4"> My background is in business informatics, with a strong interest in how digital systems shape human behavior, perception, and decision-making.
+        Currently, I explore topics such as data bias, AI ethics, and human–technology interaction through small projects and short essays. I'm especially interested in reflective, human-centered approaches to technology rather than purely technical solutions.</p>
+       
+       <p className="mb-4"> Alongside this, I maintain a creative practice, using visual work as a way to think, observe, and experiment beyond formal structures.</p>
     </div>
   );
 }
